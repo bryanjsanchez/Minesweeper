effc7fb4402c2661607f6402f63cc7a8e1227f09
Bryan J Sanchez <bryanjsanchez@gmail.com>
2017-07-04 11:12:24 -0400

    Fix bug: Panel not drawn

--- Minesweeper/src/Main.java
+++ Minesweeper/src/Main.java
@@ -6,13 +6,15 @@ public class Main {
 		MyPanel myPanel = new MyPanel();
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
 		
+		myFrame.add(myPanel);
+		myFrame.addMouseListener(myMouseAdapter);
+		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(500, 200);
 		myFrame.setSize(325, 375);
 		myFrame.setResizable(false);
 		myFrame.setVisible(true);
-		myFrame.add(myPanel);
-		myFrame.addMouseListener(myMouseAdapter);
+		
 		
 		myPanel.resetButton.doClick();
 	}
